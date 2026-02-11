ee76fa57f8e7089bd830ad4bbe36912ebc9aff1a
Abhimnayu2088 <25114cn246@glbitm.ac.in>
2026-02-11 10:41:25 +0530

    abhimanyu.c

---
+++ abhimanyu.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter a=");
+    scanf("%d",&a);
+    printf("enter b=");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("\na is greater than b");
+    
+    }
+    else
+    {
+        printf("n\b is greater than a");
+    }
+}
